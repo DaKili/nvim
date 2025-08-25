5202c9fdf3b304c6fe0b60ac958195b2572e4590
DaKili <mail@kilianwimmer.de>
2025-08-25 21:45:44 +0200

    add lsp/autocompletion

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -29,7 +29,7 @@ vim.opt.completeopt = "menuone,noinsert,noselect,preview"
 vim.opt.showmode = false
 vim.opt.pumheight = 10
 vim.opt.list = true
-vim.opt.listchars = { trail = "·", nbsp = "␣" } -- TODO: Check how this can take effect
+vim.opt.listchars = { trail = "·", nbsp = "␣" }
 
 -- File handling
 vim.opt.autoread = true
