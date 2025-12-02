b0d8a5965c932c91c0d42df1a9fdcdbdb9c4a5aa
DaKili <42497623+DaKili@users.noreply.github.com>
2025-12-02 23:46:24 +0100

    disable vimade for neogit

--- lua/plugins/vimade.lua
+++ lua/plugins/vimade.lua
@@ -5,4 +5,15 @@ return {
         ncmode = 'buffers',
         fadelevel = 0.5,
     },
+    config = function(_, opts)
+        require('vimade').setup(opts)
+
+        -- Disable vimade for neogit buffers
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'NeogitStatus', 'NeogitCommitView', 'NeogitPopup', 'NeogitLogView' },
+            callback = function()
+                vim.cmd('VimadeBufDisable')
+            end,
+        })
+    end,
 }
