71c313f7b97187ba4ff32bba0eb978a2c70971f6
DaKili <42497623+DaKili@users.noreply.github.com>
2025-12-03 20:29:55 +0100

    add neogit shortcut

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,8 +1,17 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-    "ibhagwan/fzf-lua",              -- optional
-  },
+    'NeogitOrg/neogit',
+    dependencies = {
+        'nvim-lua/plenary.nvim', -- required
+        'sindrets/diffview.nvim', -- optional - Diff integration
+        'ibhagwan/fzf-lua', -- optional
+    },
+    keys = {
+        {
+            '<leader>g',
+            function()
+                require('neogit').open()
+            end,
+            desc = 'Open Neogit'
+        },
+    },
 }
