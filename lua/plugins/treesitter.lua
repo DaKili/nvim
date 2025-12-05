bc2948d833aad37fdc351c0c5f4a528fa579eb6f
Kilian Wimmer <42497623+DaKili@users.noreply.github.com>
2025-12-05 11:09:33 +0100

    neable folding for all filetypes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -43,10 +43,18 @@ return {
                 additional_vim_regex_highlighting = false,
             },
             indent = { enable = true },
-            folding = { enable = true },
         })
-        vim.opt.foldmethod = 'expr'
-        vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        vim.opt.foldenable = false
+
+        -- Enable folding via Treesitter
+        vim.api.nvim_create_autocmd('BufEnter', {
+        callback = function()
+            if vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()] then
+                vim.opt_local.foldmethod = 'expr'
+                vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.opt_local.foldenable = true
+                vim.opt_local.foldlevelstart = 99
+            end
+        end,
+    })
     end,
 }
