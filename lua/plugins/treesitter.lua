608291ef3da8d73bf2b644c4edecacb21e28a327
Kilian Wimmer <42497623+DaKili@users.noreply.github.com>
2025-12-05 10:54:55 +0100

    add angular to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -27,6 +27,7 @@ return {
     config = function()
         require('nvim-treesitter.configs').setup({
             ensure_installed = {
+                'angular',
                 'lua',
                 'javascript',
                 'typescript',
