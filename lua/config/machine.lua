9e261e7d630c854cbc0b3dd5e89cdfa4f3ae11e8
DaKili <mail@kilianwimmer.de>
2026-01-05 12:00:42 +0100

    update block comment

--- lua/config/machine.lua
+++ lua/config/machine.lua
@@ -1,21 +1,22 @@
--- Each machine enables what it needs in machine_local.lua
--- For specific configurations create 'lua/config/machine_local.lua' and specify what you want installed.
---
--- ```
--- local M = {}
--- local global_list = require("config.machine")
---
--- -- For specifics (keys of the tables in this files):
--- M.enabled_lsp = { 'lua', 'rust', 'go' }
--- M.enabled_tools = { 'prettier', 'rustfmt', 'gofmt' }
---
--- -- For everything:
--- M.enabled_lsp = vim.tbl_keys(global_list.available.lsp)
--- M.enabled_tools = vim.tbl_keys(global_list.available.tools)
---
--- return M
--- ```
+--[[
+    Each machine enables what it needs in machine_local.lua
+    For specific configurations create 'lua/config/machine_local.lua' and specify what you want installed.
 
+    ```
+    local M = {}
+    local global_list = require("config.machine")
+
+    -- For specifics (keys of the tables in this files):
+    M.enabled_lsp = { 'lua', 'rust', 'go' }
+    M.enabled_tools = { 'prettier', 'rustfmt', 'gofmt' }
+
+    -- For everything:
+    M.enabled_lsp = vim.tbl_keys(global_list.available.lsp)
+    M.enabled_tools = vim.tbl_keys(global_list.available.tools)
+
+    return M
+    ```
+--]]
 local M = {}
 
 M.available = {
