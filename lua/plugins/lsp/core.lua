76f8d3eb9ff0fc1c6ef951717b9823bf000115d3
DaKili <42497623+DaKili@users.noreply.github.com>
2026-01-05 12:41:09 +0100

    remove duplicate setup

--- lua/plugins/lsp/core.lua
+++ lua/plugins/lsp/core.lua
@@ -106,18 +106,6 @@ local function setup_server_configs()
             return root
         end,
     })
-
-    vim.lsp.config('roslyn', {
-        settings = {
-            ['csharp|inlay_hints'] = {
-                csharp_enable_inlay_hints_for_implicit_object_creation = true,
-                csharp_enable_inlay_hints_for_implicit_variable_types = true,
-            },
-            ['csharp|code_lens'] = {
-                dotnet_enable_references_code_lens = true,
-            },
-        },
-    })
 end
 
 -- Main plugin configuration
