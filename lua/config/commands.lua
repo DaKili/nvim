eb216d1af8be440d4edf9ae74c2466b3f4ec722b
Kilian Wimmer <Kilian.Wimmer@digital-experts.com>
2026-01-19 08:39:07 +0100

    add command to close saved buffers

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -14,3 +14,18 @@ vim.api.nvim_create_autocmd('BufWritePre', {
         end
     end,
 })
+
+-- Close all saved buffers
+vim.api.nvim_create_user_command('BufCleanup', function()
+    local current = vim.api.nvim_get_current_buf()
+    local closed = 0
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if buf ~= current and vim.api.nvim_buf_is_valid(buf) then
+            if vim.bo[buf].buftype == '' and not vim.bo[buf].modified then
+                vim.api.nvim_buf_delete(buf, { force = false })
+                closed = closed + 1
+            end
+        end
+    end
+    print('Closed ' .. closed .. ' saved buffer(s)')
+end, { desc = 'Close all saved buffers except current' })
