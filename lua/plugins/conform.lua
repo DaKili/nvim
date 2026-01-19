b98563b4a51db1b7e481fc21af100e5ac5b23e09
Kilian Wimmer <42497623+DaKili@users.noreply.github.com>
2026-01-19 08:38:30 +0100

    add scss to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -10,6 +10,7 @@ return {
             json = { 'prettierd', 'prettier', stop_after_first = true },
             html = { 'prettierd', 'prettier', stop_after_first = true },
             css = { 'prettierd', 'prettier', stop_after_first = true },
+            scss = { 'prettierd', 'prettier', stop_after_first = true },
             go = { 'gofumpt' },
             rust = { 'rustfmt' },
             cs = { 'csharpier' },
