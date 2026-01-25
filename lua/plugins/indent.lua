440be2512ca7dad686ab431db60eb0c15cfa6a35
DaKili <42497623+DaKili@users.noreply.github.com>
2026-01-25 18:21:17 +0100

    add indent configuration

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,8 +1,18 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
     ---@module "ibl"
     opts = {
-        indent = { char = '▏'},
-    }
+        indent = {
+            char = '▏',
+            tab_char = '▏',
+        },
+        scope = { enabled = true },
+    },
+    config = function(_, opts)
+        vim.api.nvim_set_hl(0, 'IblIndent', { fg = '#3a3a3a' })
+        vim.api.nvim_set_hl(0, 'IblScope', { fg = '#ee5396' })
+
+        require('ibl').setup(opts)
+    end,
 }
