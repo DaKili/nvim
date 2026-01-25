02a64aff1e992fc8be7198d6ea4b0f16957583dd
Kilian Wimmer <Kilian.Wimmer@digital-experts.com>
2026-01-25 15:11:28 +0100

    close obsolete files before saving

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -6,6 +6,17 @@ return {
     end,
     opts = {
         suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+        pre_save_cmds = {
+            -- Close any buffers without names before saving
+            function()
+                for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                    if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_name(buf) == '' then
+                        vim.api.nvim_buf_delete(buf, { force = true })
+                    end
+                end
+            end,
+        },
+        log_level = 'error',
     },
     keys = {
         {
