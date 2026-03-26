c7cd18dc41c9b377612686bef38ff77b663cfc2d
DaKili <42497623+DaKili@users.noreply.github.com>
2026-03-26 12:07:03 +0100

    feat: allow oxfmt to read .prerrierrc files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,15 +2,29 @@ return {
     'stevearc/conform.nvim',
     opts = {
         async = true,
+        formatters = {
+            oxfmt = {
+                prepend_args = function()
+                    local prettierrc = vim.fs.find('.prettierrc', {
+                        upward = true,
+                        path = vim.fn.expand('%:p:h'),
+                    })[1]
+                    if prettierrc then
+                        return { '-c', prettierrc }
+                    end
+                    return {}
+                end,
+            },
+        },
         formatters_by_ft = {
             lua = { 'stylua' },
-            javascript = { 'prettierd', 'prettier', stop_after_first = true },
-            typescript = { 'prettierd', 'prettier', stop_after_first = true },
-            typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-            json = { 'prettierd', 'prettier', stop_after_first = true },
-            html = { 'prettierd', 'prettier', stop_after_first = true },
-            css = { 'prettierd', 'prettier', stop_after_first = true },
-            scss = { 'prettierd', 'prettier', stop_after_first = true },
+            javascript = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+            typescript = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+            typescriptreact = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+            json = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+            html = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+            css = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
+            scss = { 'oxfmt', 'prettierd', 'prettier', stop_after_first = true },
             go = { 'gofumpt' },
             rust = { 'rustfmt' },
             cs = { 'roslyn' },
