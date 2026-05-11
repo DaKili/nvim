66345f8016d17e6c1fdbc83177b4ba660c1d534f
DaKili <42497623+DaKili@users.noreply.github.com>
2026-05-11 08:10:40 +0200

    fix nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,38 +1,27 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
+    branch = 'main',
     build = ':TSUpdate',
     lazy = false,
-    cond = function() -- Only load if a C compiler is available
-        local compilers = { 'cc', 'gcc', 'clang', 'cl', 'zig' }
-        for _, compiler in ipairs(compilers) do
-            if vim.fn.executable(compiler) == 1 then
-                return true
-            end
-        end
-        return false
-    end,
     config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = {
-                'angular',
-                'css',
-                'scss',
-                'html',
-                'javascript',
-                'json',
-                'go',
-                'lua',
-                'tsx',
-                'typescript',
-                'yaml',
-            },
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
+        vim.env.CC = 'gcc'
+        require('nvim-treesitter').install({
+            'angular',
+            'css',
+            'scss',
+            'html',
+            'javascript',
+            'json',
+            'go',
+            'lua',
+            'tsx',
+            'typescript',
+            'yaml',
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function(ev)
+                pcall(vim.treesitter.start, ev.buf)
+            end,
         })
     end,
 }
